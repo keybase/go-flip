780b04b2c896ab514f5fd4e8c5478d5ea159d798
Maxwell Krohn <themax@gmail.com>
2019-02-05 14:55:32 -0500

    dealer test

---
+++ dealer_test.go
@@ -0,0 +1,30 @@
+package flip
+
+import (
+	"context"
+	"fmt"
+	clockwork "github.com/keybase/clockwork"
+	"testing"
+)
+
+type testDealersHelper struct {
+	clock clockwork.FakeClock
+}
+
+func newTestDealersHelper() *testDealersHelper {
+	return &testDealersHelper{clock: clockwork.NewFakeClock()}
+}
+
+func (t *testDealersHelper) Clock() clockwork.Clock {
+	return t.clock
+}
+
+func (t *testDealersHelper) CLogf(ctx context.Context, fmtString string, args ...interface{}) {
+	fmt.Printf(fmtString, args...)
+}
+
+func TestDealer(t *testing.T) {
+	dh := newTestDealersHelper()
+	dealer := NewDealer(dh)
+	<-dealer.UpdateCh()
+}
